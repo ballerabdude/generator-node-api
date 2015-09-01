42a256e3f05eda0ee4aea03f20d4e8d728d46bad
Abdul Hagi <Abdul.hagi@hotmail.com.com>
2015-09-01 17:36:07 -0400

    Updated file name

--- generators/scaffold/templates/_controller.js
+++ generators/scaffold/templates/_controller.js
@@ -1,5 +1,5 @@
 'use strict';
-let <%= scaffoldName.capital  %>Model = require('../models/<%= scaffoldName.capital  %>Model');
+let <%= scaffoldName.capital  %>Model = require('../models/<%= scaffoldName.lower  %>Model');
 let Boom = require('boom');
 module.exports = class <%= scaffoldName.capital  %>Controller {
   constructor() {
